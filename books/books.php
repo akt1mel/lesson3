00b1d68e8756716160ab9f2a5de3c83727b4d561
Akt1mel <Kostyavas.94@mail.ru>
2018-08-24 12:47:56 +0300

    Обработка ошибок

--- books/books.php
+++ books/books.php
@@ -1,21 +1,46 @@
 <?php
 
-$query = implode(" ", array_slice($argv, 1)); //Считываем введенное название и записываем в строку.
+if ($argc < 2) {
+  echo "Введите название книги";
+} else {
+  $query = implode(" ", array_slice($argv, 1)); //Считываем введенное название и записываем в строку.
 
-$url = "https://www.googleapis.com/books/v1/volumes?q=".urlencode($query);
+  $url = "https://www.googleapis.com/books/v1/volumes?q=".urlencode($query);
 
-$f = json_decode(file_get_contents($url), true);
+  $booksData = json_decode(file_get_contents($url), true);
 
-if (!json_last_error_msg()) {  //Выводим ошибку запроса json, если она существует
-  echo json_last_error_msg();
-}
-
-$file = fopen('books.csv', 'w');
-  foreach ($f["items"] as $book) {
-    if (isset($book["volumeInfo"]["authors"][0])) {
-      fputcsv($file, [$book["volumeInfo"]["title"], $book["volumeInfo"]["authors"][0]]);
-    } else { // Если автора нет, записываем Wihout authors
-      fputcsv($file, [$book["volumeInfo"]["title"], "Without authors"]);
+  switch (json_last_error()) {
+        case JSON_ERROR_DEPTH:
+            echo ' - Достигнута максимальная глубина стека';
+        break;
+        case JSON_ERROR_STATE_MISMATCH:
+            echo ' - Некорректные разряды или несоответствие режимов';
+        break;
+        case JSON_ERROR_CTRL_CHAR:
+            echo ' - Некорректный управляющий символ';
+        break;
+        case JSON_ERROR_SYNTAX:
+            echo ' - Синтаксическая ошибка, некорректный JSON';
+        break;
+        case JSON_ERROR_UTF8:
+            echo ' - Некорректные символы UTF-8, возможно неверно закодирован';
+        break;
     }
-  }
-fclose($file);
+
+
+
+  $file = fopen('books.csv', 'w');
+    if ($booksData["totalItems"] == 0) {
+      echo "Данных по книгам с таким названием нет.";
+    } else {
+      foreach ($booksData["items"] as $book) {
+        if (isset($book["volumeInfo"]["authors"][0])) {
+          fputcsv($file, [$book["volumeInfo"]["title"], $book["volumeInfo"]["authors"][0]]);
+        } else { // Если автора нет, записываем Wihout authors
+          fputcsv($file, [$book["volumeInfo"]["title"], "Without authors"]);
+        }
+      }
+    fclose($file);
+    }
+
+}
